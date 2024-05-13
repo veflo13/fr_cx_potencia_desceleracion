af634d7a3aa271047ad25ac04e2c5ae662a1cf6f
Verónica Flores Gutiérrez <113630553+veflo13@users.noreply.github.com>
2024-05-13 19:25:18 +0200

    Update ensayo3.m
    corrección de derivada y cálculo de potencia

--- mov_longitudinal/ensayos/ensayo3.m
+++ mov_longitudinal/ensayos/ensayo3.m
@@ -3,81 +3,67 @@ function ensayo3()
     ws=[];
     ws=ans;
 
-    t=ws(1,:);
-    acel=ws(2,:);
-    vel=ws(3,:);
-    frglobal=ws(4,:);
-    frcrueda=ws(5,:);
+
+    load('C:\Users\Usuario_UMA\Documents\MATLAB\mov_longitudinal\data\ensayo3\result_pot.mat');
+    ws=[];
+    ws=ans;
 
 
+    t=ws(1,:); %se extrae el tiempo 
+    vel=ws(3,:); %se extrae el tiempo 
+    vel2 =vel / 3.6 ; %se pasa la vel a km/h;
+    acel = diff(vel2)./diff(t); %la derivada de la aceleracion dv/dt
+    frglobal= 0.0017; %tomo el valor obtenido del fr
+    frcrueda= 4.235932933862128e-04; %tomo el valor obtenido del fr cada rueda
 
-    frmean2=mean(frglobal);
 
-    f3=figure('Menubar','none','NumberTitle','off','Name','Ensayo 3 marcha 1',... 
+    f2=figure('Menubar','none','NumberTitle','off','Name','Ensayo 2 ',... 
        'Color',[1, 1, 1],'Resize','on');
     hold on;
 
-        subplot(4,1,1)
+     %GRÁFICAS 
+        subplot(2,1,1)
         plot (t,vel)
-        xlim ([0 10]);
-        xlabel('[s]');
-        ylabel('[km/h]');
-        title 'Información obtenida del Carsim de 20 a 40km/h con marcha en 1';
-        subtitle 'velocidad';
+
+        xlabel('Tiempo [s]');
+        ylabel('Velocidad [m/s]');
+        title 'Información obtenida del Carsim de 40 a 120km/h';
+        subtitle 'Velocidad';
+
+
     
-        
-        subplot(4,1,2);
-        plot(t,acel);
-        xlim ([0 10]);
-        xlabel('[s]');
-        ylabel('[m/s^2]');
-        subtitle 'aceleración';
-        
-        subplot (4, 1, 3);
-        plot(t,frglobal);
-        xlim ([0 10]);
-        xlabel('[s]');
-        ylabel('fr');
-        subtitle 'frglobal';
+ 
+        subplot(2,1,2);
+        plot(t(2:end), acel);
+        subtitle 'Aceleración';
+        xlabel('Aceleracion [m/s2]');
+        ylabel('tiempo [s]');
     
-        
-        subplot (4, 1, 4);
-        plot(t,frcrueda);
-        xlim ([0 10]);
-        xlabel('[s]');
-        ylabel('fr-r');   
-        subtitle 'fr cada rueda';
     hold off;
 
-    f3_c=figure('Menubar','none','NumberTitle','off','Name','Ensayo 3 cx marcha 1',... 
-       'Color',[1, 1, 1],'Resize','on', 'Position', [0, 100, 500, 500]);
+    %cálculo de potencia 
+
     m=1838.6
     A=2.8
     den=1.206
+    cx =-0.0278;
     
-    numerador = [(m*acel)+(frmean2*m*9.8)];
-    denominador = [0.5 * den * A * vel.^2];
-    cx =[numerador./denominador];
-    cxmean=mean(cx)
+
+    denominador_pot = [0.5 * den * A * vel.^2];
+    denominador_pot= denominador (2:end);
 
 
-    plot(t,cx);
-    title 'CX ensayo 3'
-    legend('cxmean=0.2201')
-    xlim ([0 10]);
 
-
-    f3_pot=figure('Menubar','none','NumberTitle','off','Name','Ensayo 3 potencia marcha 1',... 
+   f3_pot=figure('Menubar','none','NumberTitle','off','Name','Ensayo 3 potencia marcha 1',... 
        'Color',[1, 1, 1],'Resize','on', 'Position', [1000, 300, 500, 300]);
 
-    ter1=[m*acel-frglobal*m*9.8];
+    ter1=[m*acel-frglobal*m*-9.8];
     ter2=[denominador.*cx];
-    pot=[(ter1-ter2).*vel];
+    pot=[(ter1-ter2).*vel(2:end)];
     potmax=max(pot)
 
-    plot(vel,pot);
-    legend('max_pot= -0.003127')
+    plot(vel(2:end),pot);
+    legend('max_p_o_t= 0.000066686', 'Location','south')
     title 'potencia ensayo 3'
-    
 
 end
